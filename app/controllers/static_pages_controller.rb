aebc1eb334a3aa0d59e6f10b4e3f51b5cfa7037b
NDK1410 <khanh.nd1410@gmail.com>
2018-03-14 20:04:54 +0700

    layout done

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def products
+  end
+
+  def about
+  end
+
+  def blogs
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get 'clothing' => 'static_pages#clothing'
+
+  get 'news'    => 'static_pages#news'
+
+  get 'accessories'    => 'static_pages#accessories'
+
+  get 'contact'    => 'static_pages#contact'
+
+  get  'signin'  => 'users#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
